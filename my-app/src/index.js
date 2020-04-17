490d497b7ec6ac55677f3c3b374497545be62b7f
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-18 02:36:48 +0530

    index.js
    index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import Hello from './Hello';
 import NameComponent from './NameComponent';
 import ProjectManagement from './ProjectManagement';
+import EmployeeComponent from './EmployeeComponent';
+import EmployeeListComponent from './EmployeeListComponent';
 import './index.css';
+
 
 ReactDOM.render(
       <section>
-        <Hello />  <NameComponent /> <ProjectManagement/>
+        <Hello/> <NameComponent/> <ProjectManagement/> <EmployeeComponent/> <EmployeeListComponent/>
       </section>
   ,
   document.getElementById('root')
