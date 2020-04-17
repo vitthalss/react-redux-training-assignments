c9173b5a549cc9141babb822840e467ab050f1dc
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-17 20:01:14 +0530

    index.js
    added changes for ProjectManagement module

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hello from './Hello';
 import NameComponent from './NameComponent';
+import ProjectManagement from './ProjectManagement';
 import './index.css';
 
 ReactDOM.render(
       <section>
-        <Hello />  <NameComponent />
+        <Hello />  <NameComponent /> <ProjectManagement/>
       </section>
   ,
   document.getElementById('root')
