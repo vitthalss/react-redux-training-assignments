2c05159c97a3bb96f5a51a41da98e82c3e9abe5c
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-17 19:59:46 +0530

    ProjectManagement.js
    initial commit

---
+++ my-app/src/ProjectManagement.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+
+class ProjectManagement extends Component {
+  render() {
+    return (
+      <div className="center">
+        <div>
+          <h1 className="textcolor">Welcome to REACT - PROJECT MANAGEMENT LOGO!</h1>
+            <svg  width="100" height="200" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 188.5 188.5" aria-labelledby="title">
+                <title id="title">Icon</title>
+                
+                <circle id="background" className="cls-3" cx="94.2" cy="94.2" r="94.2"/>
+                <path className="cls-4" d="M50.3 69.8h10.4v72.51H50.3z"/>
+                <path fill="#353f49" d="M50.3 77.5h10.4v57.18H50.3z"/>
+                <path fill="#474f59" d="M60.7 77.5h38.9v57.19H60.7z"/>
+                <path className="cls-7" d="M60.7 69.8h38.9v7.66H60.7z"/>
+                <path className="cls-5" d="M60.7 134.7h38.9v7.66H60.7z"/>
+
+            </svg>
+        </div>
+      </div>
+ 
+    );
+  }
+}
+
+export default ProjectManagement;
