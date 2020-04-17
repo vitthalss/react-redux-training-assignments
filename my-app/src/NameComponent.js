2889b81ac537259dfe11fa97a7d06d7333ec3440
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-17 18:32:46 +0530

    NameComponent.js
    initial commit

---
+++ my-app/src/NameComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class NameComponent extends Component {
+  render() {
+    return (
+       <body>
+           <div className="Hello">
+              <div>
+                <h1>Hello Vitthal!</h1>
+              </div>
+          </div>
+      </body>
+    );
+  }
+}
+
+export default NameComponent;
