3358503376d3c1dc8aa1da38bc6ec89c6561d726
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-18 19:24:56 +0530

    index.js
    added  Q5 changes.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,12 +6,13 @@ import ProjectManagement from './ProjectManagement';
 import EmployeeComponent from './EmployeeComponent';
 import EmployeeListComponent from './EmployeeListComponent';
 import OddEvenArrayComponent from './OddEvenArrayComponent';
+import CustomerComponent from './CustomerComponent';
 import './index.css';
 
 ReactDOM.render(
       <section>
         <Hello/> <NameComponent/> <ProjectManagement/> <EmployeeComponent/> <EmployeeListComponent/>
-         <OddEvenArrayComponent/>
+         <OddEvenArrayComponent/><CustomerComponent/>
       </section>
   ,
   document.getElementById('root')
