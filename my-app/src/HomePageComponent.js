76785919938cfa03d16662ac9dacd4e56e70f456
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-20 20:26:49 +0530

    HomePageComponent.js
    initial commit

---
+++ my-app/src/HomePageComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class HomePageComponent extends Component {
+  render() {
+    return (
+      <h1>Home Page</h1>
+    );
+  }
+}
+export default HomePageComponent;
