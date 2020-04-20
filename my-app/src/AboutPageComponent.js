e6a233dc3bf0a5badc1aa2b2fc70b941f5ce188a
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-20 22:19:55 +0530

    AboutPageComponent.js
    added class

--- my-app/src/AboutPageComponent.js
+++ my-app/src/AboutPageComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class AboutPageComponent extends Component {
   render() {
     return (
-      <h1>About Page</h1>
+      <h1 className="center">About Page</h1>
     );
   }
 }
