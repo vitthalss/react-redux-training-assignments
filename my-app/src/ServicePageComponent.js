b0b08ad39f1e3a8b8a1fd562b6db1ae6471c46bb
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-20 20:27:33 +0530

    ServicePageComponent.js
    initial commit

---
+++ my-app/src/ServicePageComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ServicePageComponent extends Component {
+  render() {
+    return (
+      <h1>Service Page</h1>
+    );
+  }
+}
+export default ServicePageComponent;
