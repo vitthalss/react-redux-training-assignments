e2f59198c3a76fd4f2ffb927944d548fbc3cbbc1
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-20 20:24:46 +0530

    AboutPageComponent.js
    initial commit

---
+++ my-app/src/AboutPageComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class AboutPageComponent extends Component {
+  render() {
+    return (
+      <h1>About Page</h1>
+    );
+  }
+}
+export default AboutPageComponent;
