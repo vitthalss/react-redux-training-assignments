2d24590f71e49e820a8edf0c1601bb4b65f699c2
VITTHAL SHIRKE <vi8l.srk@gmail.com>
2020-04-20 20:25:41 +0530

    ContactPageComponent.js
    initial commit

---
+++ my-app/src/ContactPageComponent.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ContactPageComponent extends Component {
+  render() {
+    return (
+      <h1>Contact Page</h1>
+    );
+  }
+}
+export default ContactPageComponent;
